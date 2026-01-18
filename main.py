f62bbcfb6f72068c96b7b02b132e70f966a1779f
Karim Dirani <kdirani07@gmail.com>
2026-01-18 16:22:48 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+name = "Karim Dirani"
+print(name)
+
+# print the first letter from the name
+print(name[0])
+
+# print the first letter from the surname
+print(name[6])
+
+# print the first name from the "name" variable
+print(name[0:5])
+
+# print the surname from the "name" variable
+print(name[6:12])
+
+# find the number of charcters(len) in the "name" variable
+print(len(name))
+
+# string splicing
+
+x = "champion"
+
+# print the first 4 characters
+print(x[0:4])
+
+# print index number 5-7
+print(x[5:8])
+
+# print from index number 4 to the last charcter
+print(x[4: ])
+
+# print the first 5 charcters
+print(x[0:5])
+print(x[ :5])
